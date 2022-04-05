d3f09f320943a86644e6f5d6beb3a54e8e7551d1
ZaniAndrei <zaniandrei1@gmail.com>
2022-04-05 18:01:48 -0300

    Atualizaçao

--- Pilhas/Atividade2.c
+++ Pilhas/Atividade2.c
@@ -32,12 +32,10 @@ typedef struct{
   int qtde;
 }PilhaDinamica;
 //verifica a validade dos caracteres para ver se a função é terminada ou não
-bool letraValida(char caracter){
-  if ((caracter >= 65 && caracter <= 122) || (caracter == 42 ||
-    caracter == 43 || caracter == 45 || caracter == 47 )){
+bool caracterValido(char caracter){
+  if (caracter >= 65 && caracter <= 122){
     return true;
   }else{
-    printf("Caracter (%c) invalido\n", caracter);
     return false;
   }
 }
@@ -64,13 +62,11 @@ bool estaVazia(PilhaDinamica *compilador){
 
 void empilharElementos(PilhaDinamica *compilador, char caracter){
   //só ira empilhar os elementos se eles forem validos(funções retornam true)
-  if (operacaoValida(caracter) || letraValida(caracter)) {
     PtrNoPilha aux = malloc(sizeof(NoPilha));
     aux->instrucao = caracter;
     aux->proximo = compilador->topo;
     compilador->topo = aux;
     compilador->qtde++;
-  }
 }
 
 //imprimir para acompanhar o progresso do codigo
@@ -85,12 +81,20 @@ void imprimePilha(PilhaDinamica *compilador){
   printf(" ]\n");
 }
 
-void desempilhaInstrucoes(PilhaDinamica *compilador, FILE* arq){
-  PtrNoPilha aux;
-  aux = pilha->topo;
-  pilha->topo = pilha->topo->proximo;
-  free(aux);
-  pilha->qtde--;
+void desempilhaInstrucoes(PilhaDinamica *compilador, FILE* arq, char caractere){
+  while(!estaVazia(compilador)) {
+    for (size_t i = 0; i < 2; i++) {
+      char valor;
+      valor = compilador->topo->instrucao;
+      PtrNoPilha aux;
+      aux = compilador->topo;
+      compilador->topo = compilador->topo->proximo;
+      free(aux);
+      compilador->qtde--;
+      printf("Valor desempilhado: %c\n",valor);
+      fprintf(arq, "STR %c\n", valor);
+    }
+  }
 }
 
 
@@ -105,24 +109,24 @@ int main(int argc, char const *argv[]) {
     printf("ERRO NO ARQUIVO DE LEITURA\n");
     exit(1);
   }
-  while (caractere != ' ') {
-    fscanf(arq1, "%c", &caractere);
-    printf("Caractere: %c\n\n", caractere);
-    empilharElementos(&c, caractere);
-    imprimePilha(&c);
-  }
-  fclose(arq1);
-
-  while (!estaVazia(&c)) {
-    desempilhaInstrucoes(&c, arq2);
-  }
-
   FILE *arq2 = fopen("saida.txt", "w");
   if (arq2 == NULL) {
-    printf("ERRO NO ARQUIVO DE SAÍDA\n");
+    printf("ERRO NO ARQUIVO DE SAIDA\n");
     exit(1);
   }
 
+  while (caractere != ' ') {
+    fscanf(arq1, "%c", &caractere);
+    printf("Caractere: %c\n\n", caractere);
+    if (caracterValido(caractere)) {
+      empilharElementos(&c, caractere);
+    }
+    if (operacaoValida(caractere)) {
+      desempilhaInstrucoes(&c, arq2, caractere);
+    }
+    imprimePilha(&c);
+  }
+  fclose(arq1);
 
   return 0;
 }
